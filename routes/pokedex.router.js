939deeba43efeeda79592648e9ebb9f40b8f9935
Cristian Jaramillo <crjaramillohe@unal.edu.co>
2023-11-12 09:30:56 -0500

    /pokemones CRUD

---
+++ routes/pokedex.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+
+const router = express.Router();
+
+router.use(express.static(path.join(__dirname, '../public')));
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public','index.html'));
+});
+
+
+
+
+module.exports = router;
